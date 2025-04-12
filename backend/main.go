932ef934d149df075b58e4fcd5497371d80ff035
joschrew <>
2025-04-12 12:29:25 +0200

    Add backend to deliver the manifest

---
+++ backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", hello)
+	e.GET("/manifest", manifest)
+
+	e.Start(":6701")
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "hello World")
+}
+
+func manifest(c echo.Context) error {
+	return c.File("manifest.json")
+}
